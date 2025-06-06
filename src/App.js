f77d15abbf785bd4a9c3a51c1846af5c106bd8ae
anurag-2911 <anurag.hcu@gmail.com>
2025-06-06 23:01:56 +0530

    files restructure

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>YouTube Video Explorer</h1>
+      <h1>Trending...</h1>
       <Controls
         regions={regions}
         categories={categories}
