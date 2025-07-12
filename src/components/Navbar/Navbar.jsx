1e024c9255720da65cd7af8ab48f5813b17305c1
Anurag Kumar <AnKumar@flexera.com>
2025-07-12 20:22:16 +0530

    cleanup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,24 +3,45 @@ import { Link } from "react-router-dom";
 function Navbar({ regions, selectedRegion, onRegionChange }) {
   return (
     <nav className="bg-blue-700 text-white px-4 py-3 flex justify-between items-center sticky top-0 z-50 shadow">
-      <Link to="/" className="font-bold text-xl">JustScroll</Link>
+      {/* Left: Logo */}
+      <div className="flex-1">
+        <Link to="/" className="font-bold text-xl">
+          JustScroll
+        </Link>
+      </div>
 
-      <div className="flex items-center gap-4">
+      {/* Center: Nav Links */}
+      <ul className="flex-1 flex justify-center gap-6 text-md font-medium">
+        <li>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/articles" className="hover:underline">
+            Articles
+          </Link>
+        </li>
+        <li>
+          <Link to="/jokes" className="hover:underline">
+            Jokes
+          </Link>
+        </li>
+      </ul>
+
+      {/* Right: Region Selector */}
+      <div className="flex-1 flex justify-end">
         <select
           className="text-black px-2 py-1 rounded"
           value={selectedRegion}
           onChange={onRegionChange}
         >
           {regions?.map((region) => (
-            <option key={region} value={region}>{region}</option>
+            <option key={region} value={region}>
+              {region}
+            </option>
           ))}
         </select>
-
-        <ul className="hidden sm:flex gap-4">
-          <li><Link to="/" className="hover:underline">Home</Link></li>
-          <li><Link to="/articles" className="hover:underline">Articles</Link></li>
-          <li><Link to="/jokes" className="hover:underline">Jokes</Link></li>
-        </ul>
       </div>
     </nav>
   );
